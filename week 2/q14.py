5b5ddffb1988c34cf5996e9220dfbf985e322051
Ayesha Saleem <ayeshasaleem853@gmail.com>
2024-06-23 17:55:35 +0500

    day8_q14

--- week 2/q14.py
+++ week 2/q14.py
@@ -1,35 +1,27 @@
-import statistics
+class Athlete:
+    def __init__(self, name, sport):
+        self.name = name
+        self.sport = sport
+
+    def train(self):
+        print(f"{self.name} is training for {self.sport}")
 
 
-class PluginBase:
-    def load(self):
-        print("\nData Processor Plugin loaded.")
+class Musician:
+    def __init__(self, name, instrument):
+        self.name = name
+        self.instrument = instrument
 
-    def unload(self):
-        print("\nData Processor Plugin unloaded.")
-
-
-class DataProcessingMixin:
-    def process_data(self, data):
-        print("\nData processing....")
-        median = statistics.median(data)
-        return median
+    def perform(self):
+        print(f"{self.name} is performing on {self.instrument}")
 
 
-class DataProcessorPlugin(PluginBase, DataProcessingMixin):
-    def filter_data(self, data, criteria):
-        print("\nFiltering data based on criteria....")
-        for i in data:
-            if i > criteria:
-                filtered_data = [number for number in data if number < criteria]  # list comprehension
-                return filtered_data
+class Performer(Athlete, Musician):  # Multiple inheritance
+    def __init__(self, name, sport, instrument):
+        Athlete.__init__(self, name, sport)
+        Musician.__init__(self, name, instrument)
 
 
-plugin = DataProcessorPlugin()
-plugin.load()
-data = [10, 20, 30, 40, 50, 60, 70, 120]
-median_value = plugin.process_data(data)
-print(f"The median of given data is {median_value}")
-filtered_data_list = plugin.filter_data(data, 100)
-print(filtered_data_list)
-plugin.unload()
+performer = Performer("Iqra", "Basketball", "violin")
+performer.train()
+performer.perform()
