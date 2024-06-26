9f36c31d5cbef2fb621e6e54068230aff23e7770
Ayesha Saleem <ayeshasaleem853@gmail.com>
2024-06-26 13:51:38 +0500

    update_q20.py

--- week 2/q20.py
+++ week 2/q20.py
@@ -4,7 +4,6 @@ class Employee:
     @classmethod
     def increment_employee_count(cls):  # cls refers to class itself
         cls.num_of_employees += 1
-        
 
     def display_employee_count(self):  # self refers to the instance
         print(f"The number of employees are {self.num_of_employees}")
