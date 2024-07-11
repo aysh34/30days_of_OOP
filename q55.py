1d6105519320592f9b045e70a15e5ee5273d8941
aysh34 <ayeshasaleem853@gmail.com>
2024-07-11 21:10:07 +0500

    day26

--- q55.py
+++ q55.py
@@ -1,14 +1,14 @@
 # Given a list of integers, use the map function to create a new list where each integer is doubled.
 myList = [3, 4, 5, 6]
 new_list = map(lambda x: x * 2, myList)
-print(list(new_list))
+print("Problem 1: ",list(new_list))
 
 # Given two lists of numbers of the same length, use the map function to create a new list where each element is the sum of the corresponding elements from the two lists.
 
 l1 = [3, 6, 7, 9, 1]
 l2 = [13, 16, 17, 19, 1]
 sumLists = map(lambda x, y: x + y, l1, l2)
-print(list(sumLists))
+print("Problem 2: ",list(sumLists))
 
 # Given a list of tuples, where each tuple contains two numbers, use map to create a new list of the products of the numbers in each tuple. Describe how you would do this.
 nums = [(1, 2), (3, 4), (5, 6), (7, 8)]
@@ -19,10 +19,10 @@ def product(pair):
 
 
 product_list = map(product, nums)
-print(list(product_list))
+print("Problem 3: ", list(product_list))
 
 # Given a list of strings representing numbers with potential leading or trailing spaces use map to strip the spaces and convert them to integers.
 
 stringsList = [" 1 ", " 2 ", " 3 "]
 integersList = map(lambda x: int(x.strip()), stringsList)
-print(list(integersList))
+print("Problem 4: ", list(integersList))
